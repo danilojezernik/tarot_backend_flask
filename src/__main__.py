af6b76496cae3ac6de0d85adb53d399d07f3712e
danilojezernik <dani.jezernik@gmail.com>
2023-06-28 17:35:10 +0200

    tarot backend

--- src/__main__.py
+++ src/__main__.py
@@ -1,12 +1,22 @@
-from flask import Flask
+import json
+from bson.json_util import dumps
+
+from flask import Flask, json
+from flask_cors import CORS
+
+from src import db, env
 
 app = Flask(__name__)
+CORS(app)
 
 
-@app.route('/')
-def get_index():
-    return 'Hello World! Haha'
+@app.route('/api/blog', methods=['GET'])
+def get_blog():
+    objava = dumps(db.proces.objava.find())
+    return json.loads(objava)
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8004, debug=True)
+    db.drop()
+    db.seed()
+    app.run(host='0.0.0.0', port=env.PORT, debug=True)
