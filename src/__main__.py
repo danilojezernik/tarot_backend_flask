5c693c6316926916af9ce4db36fa19824d4c7395
danilojezernik <dani.jezernik@gmail.com>
2023-06-30 05:12:51 +0200

    tarot backend

--- src/__main__.py
+++ src/__main__.py
@@ -1,7 +1,9 @@
 import json
+
+from bson import ObjectId
 from bson.json_util import dumps
 
-from flask import Flask, json
+from flask import Flask, json, jsonify, request
 from flask_cors import CORS
 
 from src import db, env
@@ -16,7 +18,20 @@ def get_blog():
     return json.loads(objava)
 
 
+@app.route('/api/blog', methods=['POST'])
+def add_blog():
+    data = request.get_json()
+    db.proces.objava.insert_one(data)
+    return 'Blog added successfully'
+
+
+@app.route('/api/blog/<_id>', methods=['GET', 'POST'])
+def get_blog_id(_id):
+    objava = dumps(db.proces.objava.find_one({'_id': ObjectId(_id)}))
+    return json.loads(objava)
+
+
 if __name__ == '__main__':
-    db.drop()
-    db.seed()
+    # db.drop()
+    # db.seed()
     app.run(host='0.0.0.0', port=env.PORT, debug=True)
