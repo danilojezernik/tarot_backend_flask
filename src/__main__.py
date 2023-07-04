6a966e8c27602dea05418e9d9f7ae7c5cc8cc7f2
danilojezernik <dani.jezernik@gmail.com>
2023-07-04 21:46:09 +0200

    tarot backend

--- src/__main__.py
+++ src/__main__.py
@@ -3,13 +3,15 @@ import json
 from bson import ObjectId
 from bson.json_util import dumps
 
-from flask import Flask, json, jsonify, request
+from flask import Flask, json, jsonify, request, session
 from flask_cors import CORS
 
 from src import db, env
 
 app = Flask(__name__)
 CORS(app)
+
+app.secret_key = env.SECRET_KEY
 
 
 @app.route('/api/blog', methods=['GET'])
@@ -18,42 +20,68 @@ def get_blog():
     return json.loads(objava)
 
 
-@app.route('/api/blog', methods=['POST'])
-def add_blog():
-    if request.is_json:
-        data = request.get_json()
-        if data is not None:
-            db.proces.objava.insert_one(data)
-            return jsonify({"message": "Objava uspešno dodana!"})
-
-    return jsonify({"error": "Neveljavna zahteva ali manjkajoči podatki!"})
-
-
 @app.route('/api/blog/<_id>', methods=['GET', 'POST'])
 def get_blog_id(_id):
     objava = dumps(db.proces.objava.find_one({'_id': ObjectId(_id)}))
     return json.loads(objava)
 
 
+@app.route('/api/login', methods=['POST'])
+def login():
+    if request.is_json:
+        data = request.get_json()
+        if data is not None:
+            username = data.get('username')
+            password = data.get('password')
+
+            # Perform authentication here (e.g., check credentials against a user database)
+            if username == 'admin' and password == 'password':
+                # Authentication successful
+                session['logged_in'] = True
+                return jsonify({"message": "Login successful"})
+
+    return jsonify({"error": "Invalid login credentials"})
+
+
+@app.route('/api/blog', methods=['POST'])
+def add_blog():
+    if not session.get('logged_in'):
+        return jsonify({"error": "Unauthorized"}), 401
+    else:
+        if request.is_json:
+            data = request.get_json()
+            if data is not None:
+                db.proces.objava.insert_one(data)
+                return jsonify({"message": "Objava uspešno dodana!"})
+
+        return jsonify({"error": "Neveljavna zahteva ali manjkajoči podatki!"})
+
+
 @app.route('/api/blog/delete/<_id>', methods=['DELETE'])
 def delete_blog(_id):
-    if _id is not None:
-        db.proces.objava.delete_one({'_id': ObjectId(_id)})
-        return jsonify({"message": "Blog izbrisanm uspešno"})
+    if not session.get('logged_in'):
+        return jsonify({"error": "Unauthorized"}), 401
     else:
-        return jsonify({"error": "Blog ne obstaja"})
+        if _id is not None:
+            db.proces.objava.delete_one({'_id': ObjectId(_id)})
+            return jsonify({"message": "Blog izbrisanm uspešno"})
+        else:
+            return jsonify({"error": "Blog ne obstaja"})
 
 
 @app.route('/api/blog/edit/<_id>', methods=['POST'])
 def update_blog(_id):
-    data = request.get_json()
-    result = db.proces.objava.update_one({'_id': ObjectId(_id)}, {'$set': data})
-    if result.modified_count > 0:
-        updated_document = db.proces.objava.find_one({'_id': ObjectId(_id)})
-        updated_document['_id'] = str(updated_document['_id'])
-        return jsonify({"message": "Objava uspešno posodobljena", "updated_document": updated_document})
+    if not session.get('logged_in'):
+        return jsonify({"error": "Unauthorized"}), 401
     else:
-        return jsonify({"error": "Objava bloga ni uspela!"})
+        data = request.get_json()
+        result = db.proces.objava.update_one({'_id': ObjectId(_id)}, {'$set': data})
+        if result.modified_count > 0:
+            updated_document = db.proces.objava.find_one({'_id': ObjectId(_id)})
+            updated_document['_id'] = str(updated_document['_id'])
+            return jsonify({"message": "Objava uspešno posodobljena", "updated_document": updated_document})
+        else:
+            return jsonify({"error": "Objava bloga ni uspela!"})
 
 
 if __name__ == '__main__':
--- src/db.py
+++ src/db.py
@@ -4,6 +4,7 @@ from dataclasses import asdict
 from pymongo import MongoClient
 
 from src import env
+from src.domain.admin import Admin
 from src.domain.objava import Objava
 
 client = MongoClient(env.DB_CONNECTION)
@@ -30,6 +31,10 @@ objava = [
                   vsebina='Morbi aliquam rutrum cursus. Donec id nisl id turpis rutrum condimentum ac et elit. Maecenas semper, magna non fermentum euismod, orci orci vulputate ligula, a hendrerit ex velit eget nibh. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed eu purus scelerisque, porta quam eget, rutrum arcu. Interdum et malesuada fames ac ante ipsum primis in faucibus. Sed eros eros, tristique et tristique vitae, pretium at est. Cras efficitur est a magna interdum suscipit. Sed blandit finibus eleifend. Pellentesque nunc est, dictum ac dolor vel, lobortis consectetur libero. Nullam quis risus volutpat, posuere felis in, placerat tellus. Vestibulum odio turpis, commodo ac neque eu, commodo vulputate tortor. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Curabitur purus nulla, imperdiet id imperdiet in, sodales eu turpis.',
                   slika='slika',
                   objavljeno=datetime.datetime.now())),
+]
+
+login = [
+    asdict(Admin(uporabnisko_ime=env.UPORABNIK, geslo=env.GESLO))
 ]
 
 
